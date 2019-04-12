40c49c46a7dbff7dc466f15e3d2b117b7b76573e
nandha1nks <46973715+nandha1nks@users.noreply.github.com>
2019-04-12 08:32:38 +0530

    Update Q1_a.asm

--- Q1_a.asm
+++ Q1_a.asm
@@ -1,18 +1,18 @@
 main:
 	addi $sp,$sp,-16
 	sw $s0,0($sp)
-  sw $s1,4($sp)
-  sw $s2,8($sp)
+  	sw $s1,4($sp)
+  	sw $s2,8($sp)
 	sw $s3,12($sp)
 	addi $s0,$zero,10
-  addi $s1,$zero,5
+  	addi $s1,$zero,5
 	addi $s2,$zero,4
-  addi $s3,$zero,0
+  	addi $s3,$zero,0
   
 	add $a0,$zero,$s1
 	add $a1,$zero,$s2
 	
-  jal f1
+  	jal f1
 	div $s3,$v0,$a1
 	add $s3,$s0,$s3
   
