1960251ce81bc0b03c1c654f463eda9cd5eb820d
nandha1nks <46973715+nandha1nks@users.noreply.github.com>
2019-04-12 08:34:14 +0530

    Update Q3_a.asm

--- Q3_a.asm
+++ Q3_a.asm
@@ -6,14 +6,14 @@ loop1:
 	  addi $sp,$sp,-4
 	  sw $ra,0($sp)
 	  jal loop2
-  	mul $v0,$v0,$v0
+  	  mul $v0,$v0,$v0
 	  mul $v0,$v0,$v0
 	  addi $v0,$v0,3
 	  lw $ra,0($sp)
 	  addi $sp,$sp,4
 
 loop2:
-    addi $sp,$sp,-4
+    	  addi $sp,$sp,-4
 	  sw $ra,0($sp)
 	  jal loop3
 	  mul $t0,$v0,$v0
@@ -23,7 +23,7 @@ loop2:
 	  addi $sp,$sp,4
 
 loop3:
-    addi $sp,$sp,-4
+    	  addi $sp,$sp,-4
 	  sw $ra,0($sp)
 	  jal loop4
 	  mul $t0,$v0,$v0
@@ -33,7 +33,7 @@ loop3:
 	  addi $sp,$sp,4
 
 loop4:
-    addi $sp,$sp,-4
+    	  addi $sp,$sp,-4
 	  sw $ra,0($sp)
 	  mul $v0,$a0,7
 	  lw $ra,0($sp)
